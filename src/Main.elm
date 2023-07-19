8c31da73a533b24c5f97a8a2250a223a47ad556e
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-07-19 17:17:15 +0900

    show JSON content

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,77 @@
 module Main exposing (..)
 
-import Html exposing (Html, text)
+import Browser
+import Html exposing (Html, div, text)
+import Http
+import Json.Decode as Decode exposing (Decoder)
 
 
-main : Html msg
+type alias Person =
+    { name : String
+    , age : Int
+    }
+
+
+type alias Model =
+    Maybe Person
+
+
+type Msg
+    = GotPerson (Result Http.Error Person)
+
+
+personDecoder : Decoder Person
+personDecoder =
+    Decode.map2 Person
+        (Decode.field "name" Decode.string)
+        (Decode.field "age" Decode.int)
+
+
+getPerson : Cmd Msg
+getPerson =
+    Http.get
+        { url = "/data.json"
+        , expect = Http.expectJson GotPerson personDecoder
+        }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Nothing, getPerson )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        GotPerson result ->
+            case result of
+                Ok person ->
+                    ( Just person, Cmd.none )
+
+                Err _ ->
+                    ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ case model of
+            Just person ->
+                div []
+                    [ text ("Name: " ++ person.name)
+                    , text ("Age: " ++ String.fromInt person.age)
+                    ]
+
+            Nothing ->
+                text "Loading..."
+        ]
+
+
+main : Program () Model Msg
 main =
-    text "Hello, World!"
-
-
-add : Int -> Int -> Int
-add x y =
-    x + y
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
