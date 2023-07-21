d861ebb5ce8152a45e198152913755a183340b0c
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-07-22 00:43:20 +0900

    define repositoryStat Decoder

--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,7 @@ import Browser
 import Html exposing (Html, div, text)
 import Http
 import Json.Decode as Decode exposing (Decoder)
+import Json.Decode.Pipeline exposing (required)
 import Svg exposing (rect, svg)
 import Svg.Attributes exposing (..)
 
@@ -14,6 +15,59 @@ type alias Person =
     }
 
 
+type alias RepositoryStat =
+    { id : String
+    , name : String
+    , isPrivate : Bool
+    , isFork : Bool
+    , isArchived : Bool
+    , isTemplate : Bool
+    , diskUsage : Int
+    , pushedAt : String
+    , topics : List String
+    , languages : List RepositoryLanguage
+    , totalCommitCount : Int
+    , periodCommitCount : Int
+    }
+
+
+repositoryStatDecoder : Decoder RepositoryStat
+repositoryStatDecoder =
+    Decode.succeed RepositoryStat
+        |> required "id" Decode.string
+        |> required "name" Decode.string
+        |> required "isPrivate" Decode.bool
+        |> required "isFork" Decode.bool
+        |> required "isArchived" Decode.bool
+        |> required "isTemplate" Decode.bool
+        |> required "diskUsage" Decode.int
+        |> required "pushedAt" Decode.string
+        |> required "topics" (Decode.list Decode.string)
+        |> required "languages" (Decode.list repositoryLanguageDecoder)
+        |> required "totalCommitCount" Decode.int
+        |> required "periodCommitCount" Decode.int
+
+
+repositoryStatListDecoder : Decoder (List RepositoryStat)
+repositoryStatListDecoder =
+    Decode.list repositoryStatDecoder
+
+
+type alias RepositoryLanguage =
+    { name : String
+    , color : String
+    , size : Int
+    }
+
+
+repositoryLanguageDecoder : Decoder RepositoryLanguage
+repositoryLanguageDecoder =
+    Decode.succeed RepositoryLanguage
+        |> required "name" Decode.string
+        |> required "color" Decode.string
+        |> required "size" Decode.int
+
+
 type alias Model =
     Maybe Person
 
@@ -22,17 +76,28 @@ type Msg
     = GotPerson (Result Http.Error Person)
 
 
-personDecoder : Decoder Person
+personDecoder : Decode.Decoder Person
 personDecoder =
     Decode.map2 Person
         (Decode.field "name" Decode.string)
         (Decode.field "age" Decode.int)
 
 
+dog : String -> Int -> { name : String, age : Int }
+dog name age =
+    { name = name, age = age }
+
+
+dogDecoder =
+    Decode.succeed dog
+        |> required "name" Decode.string
+        |> required "age" Decode.int
+
+
 getPerson : Cmd Msg
 getPerson =
     Http.get
-        { url = "/data.json"
+        { url = "/data/github_stats.json"
         , expect = Http.expectJson GotPerson personDecoder
         }
 
